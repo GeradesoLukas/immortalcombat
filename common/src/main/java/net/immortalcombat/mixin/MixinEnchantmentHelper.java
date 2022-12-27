0da63d3f65dc0b8cb1729cd91c63b0815867a41b
GeradesoLukas <59490449+GeradesoLukas@users.noreply.github.com>
2022-12-27 21:11:26 +0100

    Some storing of the enchantment to modify it

--- common/src/main/java/net/immortalcombat/mixin/MixinEnchantmentHelper.java
+++ common/src/main/java/net/immortalcombat/mixin/MixinEnchantmentHelper.java
@@ -8,17 +8,32 @@ import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Mixin(EnchantmentHelper.class)
 public class MixinEnchantmentHelper {
 
+    private static Enchantment enchantment;
+
+    @ModifyVariable(
+            method = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;getAvailableEnchantmentResults(ILnet/minecraft/world/item/ItemStack;Z)Ljava/util/List;",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/Enchantment;isTreasureOnly()Z")
+    )
+    private static Enchantment captureEnchantment(Enchantment originalenchantment) {
+        enchantment = originalenchantment;
+        return originalenchantment;
+    }
+
     @ModifyExpressionValue(
             method = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;getAvailableEnchantmentResults(ILnet/minecraft/world/item/ItemStack;Z)Ljava/util/List;",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentCategory;canEnchant(Lnet/minecraft/world/item/Item;)Z"))
-    private static boolean modifyPose(boolean original, int i, ItemStack itemStack, boolean bl, ArrayList<EnchantmentInstance> list,  Item item, boolean bl2, Enchantment enchantment) {
+    private static boolean modifyCondition(boolean original, int i, ItemStack itemStack) {
         if(enchantment.canEnchant(itemStack)) {
             System.out.println("Heya");
                 return true;
